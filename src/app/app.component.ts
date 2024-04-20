3b68f8c61802bbdee664d249babd9a7a73b961f7
Aarthika008 <aarthika@student.tce.edu>
2024-04-20 14:58:32 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,28 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: '<angular-slickgrid gridId="grid1" [columnDefinitions]="columnDefinitions" [gridOptions]="gridOptions" [dataset]="dataset"></angular-slickgrid>',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
+ 
   title = 'slickgridtest';
+  columnDefinitions = [
+    { id: 'id', name: 'ID', field: 'id', sortable: true },
+    { id: 'title', name: 'Title', field: 'title', sortable: true },
+    { id: 'duration', name: 'Duration', field: 'duration', sortable: true }
+  ];
+
+  gridOptions = {
+    enableAutoResize: true,
+    enableCellNavigation: true,
+    enableSorting: true
+  };
+
+  dataset =  [
+    { id: 1, title: 'Task 1', duration: 5 },
+    { id: 2, title: 'Task 2', duration: 10 },
+    { id: 3, title: 'Task 3', duration: 15 },
+  ];;
 }
